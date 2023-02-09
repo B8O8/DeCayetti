a1b7a9c67782a179823a841916ada85367651c90
B8O8 <charbel_makhlouf76@live.com>
2023-02-09 16:56:23 +0200

    De Cayetti

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import decayetti from './DeCayetti.jpeg'
+
+function App() {
+  return (
+    <div>
+      
+      <img src={decayetti} alt="de cayetti" />
+      <h2>UNDER RENOVATION</h2>
+    </div>
+  );
+}
+
+export default App;
